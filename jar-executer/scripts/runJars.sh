bf369233fb89017b335b62fc343392c4fbbff1b3
leijiangping <leijiang21@126.com>
2019-03-23 19:47:58 +0800

    3

--- jar-executer/scripts/runJars.sh
+++ jar-executer/scripts/runJars.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-rootDir=/apps/jars/
-for file in $rootDir/*; 
+rootDir=/apps/jars
+LOG_PATH=/apps/logs
+for element in `ls $rootDir| grep .jar`
 do
-  echo $file
+  dir_or_file=$rootDir"/"$element
+  echo $dir_or_file
+  exec nohup java -jar $dir_or_file >> $LOG_PATH/nohup.out  &
+done
+echo "Success"
+#永久不关闭
+while [ true ];
+do
+  sleep 1000
+  echo $(date)
 done
