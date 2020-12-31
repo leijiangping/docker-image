66b820744f6d70820792e6ac3b86ccebf4c8849e
leijiangping <leijiang21@126.com>
2020-12-31 09:50:38 +0800

    Update runJars.sh

--- jar-executer/scripts/runJars.sh
+++ jar-executer/scripts/runJars.sh
@@ -14,12 +14,12 @@ fi;
 #输出jar应用包数量
 appCount=0
 ls  $rootDir | grep  .jar |wc -l
-JAVA_OPTS=" -Xms512m -Xmx512m "
+JAVA_OPTS=" -Xms512m -Xmx512m -Dfile.encoding=utf-8"
 #运行JARS
 for element in `ls $rootDir| grep .jar`
 do
   dir_or_file=$rootDir"/"$element
-  java -server $JAVA_OPTS  -jar $dir_or_file  &
+  java -server $JAVA_OPTS   -jar $dir_or_file  &
   #exec nohup java -jar $dir_or_file >> $LOG_PATH/nohup.out  &
   echo "应用:" $dir_or_file "启动完毕!"
   appCount=`expr $appCount + 1`
